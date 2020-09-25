8dc2db4a055d1ce6c6d1a2b23fe7f909041608b6
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-09-25 11:45:45 +1000

    Removed old commented out lines.

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
-#    version=versioneer.get_version(),
-#    cmdclass=versioneer.get_cmdclass(),
-
 setup(
     name="sungc",
     description=(
